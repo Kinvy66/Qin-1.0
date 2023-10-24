751326eaa9a1ad75de776676e37b074a16fc602d
Kinvy <kinvy66@163.com>
2023-10-24 09:47:34 +0800

    feat: 保护模式

--- boot/loader.asm
+++ boot/loader.asm
@@ -1,29 +1,86 @@
 %include "boot.inc"
 section loader vstart=LOADER_BASE_ADDR
+LOADER_STACK_TOP equ LOADER_BASE_ADDR
+jmp loader_start
 
-; 输出背景色绿色，前景色红色，并且跳动的字符串"1 MBR"
-mov byte [gs:0x00],'2'
-mov byte [gs:0x01],0xA4     ; A表示绿色背景闪烁，4表示前景色为红色
+; 构建gdt
 
-mov byte [gs:0x02],' '
-mov byte [gs:0x03],0xA4
+GDT_BASE:   dd 0x00000000
+            dd 0x00000000
+CODE_DESC:  dd 0x0000FFFF
+            dd DESC_CODE_HIGH4
+DATA_DESC:  dd 0x0000FFFF
+            dd DESC_DATA_HIGH4
+VIDEO_DESC: dd 0x80000007
+            dd DESC_VIDEO_HIGH4
 
-mov byte [gs:0x04],'L'
-mov byte [gs:0x05],0xA4   
+GDT_SIZE    equ ($ - GDT_BASE)
+GDT_LIMIT   equ (GDT_SIZE - 1)
+times 60 dq 0   ; 预留60个描述符的slot
+SELECTOR_CODE equ (0x0001 << 3) + TI_GDT + RPL0
+SELECTOR_DATA equ (0x0002 << 3) + TI_GDT + RPL0
+SELECTOR_VIDEO equ (0x0003 << 3) + TI_GDT + RPL0
 
-mov byte [gs:0x06],'O'
-mov byte [gs:0x07],0xA4
+gdt_ptr dw GDT_LIMIT
+        dd GDT_BASE
+loadermsg db '2 loader in real.'
 
-mov byte [gs:0x08],'A'
-mov byte [gs:0x09],0xA4
+loader_start:
+;---------------------------------------------------
+; INT 0x10      功能号:0x13     功能描述: 打印字符串
+;---------------------------------------------------
+;输入:
+;AH 子功能号=13H
+;BH = 页码
+;BL = 属性(若AL=00H或01H)
+;CX＝字符串长度
+;(DH、DL)＝坐标(行、列)
+;ES:BP＝字符串地址 
+;AL＝显示输出方式
+;   0——字符串中只含显示字符，其显示属性在BL中。显示后，光标位置不变
+;   1——字符串中只含显示字符，其显示属性在BL中。显示后，光标位置改变
+;   2——字符串中含显示字符和显示属性。显示后，光标位置不变
+;   3——字符串中含显示字符和显示属性。显示后，光标位置改变
+;无返回值
+    mov sp, LOADER_BASE_ADDR
+    mov bp,loadermsg
+    mov cx, 17
+    mov ax, 0x1301
+    mov bx, 0x001f
+    mov dx, 0x1800
+    int 0x10
 
-mov byte [gs:0x0a],'D'
-mov byte [gs:0x0b],0xA4
+;--------------- 准备进入保护模式 ------------------
+; 1. 打开A20
+; 2. 加载gdt
+; 3. 将cr0的PE位置1
+;-------------------------------------------------
 
-mov byte [gs:0x0c],'E'
-mov byte [gs:0x0d],0xA4
+    ;------------- 打开A20 --------------
+    in al, 0x92
+    or al,0x0000_0010
+    out 0x92, al
 
-mov byte [gs:0x0e],'R'
-mov byte [gs:0x0f],0xA4
+    ;----------- 加载gdt -----------------
+    lgdt [gdt_ptr]
 
-jmp $		       ; 通过死循环使程序悬停在此
+    ;----------- cr0 PE位置1 -------------
+    mov eax, cr0
+    or eax, 0x00000001
+    mov cr0, eax
+
+    jmp dword SELECTOR_CODE:p_mode_start    ; 跳转，刷新指令流水线
+
+[bits 32]
+p_mode_start:
+    mov ax, SELECTOR_DATA
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov esp, LOADER_STACK_TOP
+    mov ax, SELECTOR_VIDEO
+    mov gs, ax
+
+    mov byte [gs:160], 'P'
+
+    jmp $		       ; 通过死循环使程序悬停在此
--- makefile
+++ makefile
@@ -16,7 +16,7 @@ $(BUILD)/master.img: $(BUILD)/boot/boot.bin \
 
 	yes | bximage -q -hd=16 -func=create -sectsize=512 -imgmode=flat $@
 	dd if=$(BUILD)/boot/boot.bin of=$@ bs=512 count=1 conv=notrunc
-	dd if=$(BUILD)/boot/loader.bin of=$@ bs=512 count=1 seek=2 conv=notrunc
+	dd if=$(BUILD)/boot/loader.bin of=$@ bs=512 count=4 seek=2 conv=notrunc
 
 
 .PHONY: bochs
