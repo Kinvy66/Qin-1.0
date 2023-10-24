4035bf6628bb71571609ea1b31e1fa8aa084134e
Kinvy <kinvy66@163.com>
2023-10-24 11:26:55 +0800

    feat: 开启分页模式

--- boot/loader.asm
+++ boot/loader.asm
@@ -1,7 +1,6 @@
 %include "boot.inc"
 section loader vstart=LOADER_BASE_ADDR
 LOADER_STACK_TOP equ LOADER_BASE_ADDR
-jmp loader_start
 
 ; 构建gdt
 
@@ -21,9 +20,19 @@ SELECTOR_CODE equ (0x0001 << 3) + TI_GDT + RPL0
 SELECTOR_DATA equ (0x0002 << 3) + TI_GDT + RPL0
 SELECTOR_VIDEO equ (0x0003 << 3) + TI_GDT + RPL0
 
-gdt_ptr dw GDT_LIMIT
-        dd GDT_BASE
-loadermsg db '2 loader in real.'
+; total_mem_bytes用于保存内存容量,以字节为单位,此位置比较好记。
+; 当前偏移loader.bin文件头0x200字节,loader.bin的加载地址是0x900,
+; 故total_mem_bytes内存中的地址是0xb00.将来在内核中咱们会引用此地址
+total_mem_bytes dd 0					 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;以下是定义gdt的指针，前2字节是gdt界限，后4字节是gdt起始地址
+gdt_ptr  dw  GDT_LIMIT 
+    dd  GDT_BASE
+
+;人工对齐:total_mem_bytes4字节+gdt_ptr6字节+ards_buf244字节+ards_nr2,共256字节
+ards_buf times 244 db 0
+ards_nr dw 0		      ;用于记录ards结构体数量
 
 loader_start:
 ;---------------------------------------------------
@@ -42,13 +51,49 @@ loader_start:
 ;   2——字符串中含显示字符和显示属性。显示后，光标位置不变
 ;   3——字符串中含显示字符和显示属性。显示后，光标位置改变
 ;无返回值
-    mov sp, LOADER_BASE_ADDR
-    mov bp,loadermsg
-    mov cx, 17
-    mov ax, 0x1301
-    mov bx, 0x001f
-    mov dx, 0x1800
-    int 0x10
+    ; mov sp, LOADER_BASE_ADDR
+    ; mov bp,loadermsg
+    ; mov cx, 17
+    ; mov ax, 0x1301
+    ; mov bx, 0x001f
+    ; mov dx, 0x1800
+    ; int 0x10
+; loadermsg db '2 loader in real.'
+
+; xchg bx,bx
+;-------  int 15h eax = 0000E820h ,edx = 534D4150h ('SMAP') 获取内存布局  -------
+
+   xor ebx, ebx		      ;第一次调用时，ebx值要为0
+   mov edx, 0x534d4150	      ;edx只赋值一次，循环体中不会改变
+   mov di, ards_buf	      ;ards结构缓冲区
+.e820_mem_get_loop:	      ;循环获取每个ARDS内存范围描述结构
+   mov eax, 0x0000e820	      ;执行int 0x15后,eax值变为0x534d4150,所以每次执行int前都要更新为子功能号。
+   mov ecx, 20		      ;ARDS地址范围描述符结构大小是20字节
+   int 0x15
+   jc .error_hlt         ;若cf位为1则有错误发生，挂起
+   add di, cx		      ;使di增加20字节指向缓冲区中新的ARDS结构位置
+   inc word [ards_nr]	      ;记录ARDS数量
+   cmp ebx, 0		      ;若ebx为0且cf不为1,这说明ards全部返回，当前已是最后一个
+   jnz .e820_mem_get_loop
+
+;在所有ards结构中，找出(base_add_low + length_low)的最大值，即内存的容量。
+   mov cx, [ards_nr]	      ;遍历每一个ARDS结构体,循环次数是ARDS的数量
+   mov ebx, ards_buf 
+   xor edx, edx		      ;edx为最大的内存容量,在此先清0
+.find_max_mem_area:	      ;无须判断type是否为1,最大的内存块一定是可被使用
+   mov eax, [ebx]	      ;base_add_low
+   add eax, [ebx+8]	      ;length_low
+   add ebx, 20		      ;指向缓冲区中下一个ARDS结构
+   cmp edx, eax		      ;冒泡排序，找出最大,edx寄存器始终是最大的内存容量
+   jge .next_ards
+   mov edx, eax		      ;edx为总内存大小
+.next_ards:
+   loop .find_max_mem_area
+   jmp .mem_get_ok
+
+.mem_get_ok:
+   mov [total_mem_bytes], edx	 ;将内存换为byte单位后存入total_mem_bytes处。
+
 
 ;--------------- 准备进入保护模式 ------------------
 ; 1. 打开A20
@@ -71,6 +116,10 @@ loader_start:
 
     jmp dword SELECTOR_CODE:p_mode_start    ; 跳转，刷新指令流水线
 
+; TODO 这段必须放在这里，放在.mem_get_ok 下面会出错
+.error_hlt:		      ;出错则挂起
+   hlt
+
 [bits 32]
 p_mode_start:
     mov ax, SELECTOR_DATA
@@ -81,6 +130,87 @@ p_mode_start:
     mov ax, SELECTOR_VIDEO
     mov gs, ax
 
-    mov byte [gs:160], 'P'
+    ; xchg bx, bx
+    ; 创建页目录及页表并初始化页内存位图
+    call setup_page
+
+    ; ;要将描述符表地址及偏移量写入内存gdt_ptr,一会用新地址重新加载
+    sgdt [gdt_ptr]	      ; 存储到原来gdt所有的位置
+
+    ; ;将gdt描述符中视频段描述符的段基址+0xc0000000
+    mov ebx, [gdt_ptr+2]
+    or dword [ebx + 0x18 + 4], 0xc0000000  ;视频段是第3个段描述符,每个描述符是8字节,故0x18。
+	; 				      ;段描述符的高4字节的最高位是段基址的31~24位
+    ; ; 将gdt的基址加上0xc0000000使其成为内核所在的高地址
+    add dword [gdt_ptr + 2], 0xc0000000
+
+    add esp, 0xc0000000
+
+    ; 把页目录地址赋值给cr3
+    mov eax, PAGE_DIR_TABLE_POS
+    mov cr3, eax
+
+    ; 打开cr0的PG位（31）
+    mov eax, cr0
+    or eax, 0x80000000
+    mov cr0, eax
+
+    ; 开启分页后，重新加载gdt
+    lgdt [gdt_ptr]
+
+    mov byte [gs:160], 'V'                    
 
     jmp $		       ; 通过死循环使程序悬停在此
+
+;--------------------------------------------------------------
+; 函数: 创建页目录及页表
+;--------------------------------------------------------------
+setup_page:
+;先把页目录占用的空间逐字节清0
+   mov ecx, 4096
+   mov esi, 0
+.clear_page_dir:
+   mov byte [PAGE_DIR_TABLE_POS + esi], 0
+   inc esi
+   loop .clear_page_dir
+
+;开始创建页目录项(PDE)
+.create_pde:				     ; 创建Page Directory Entry
+   mov eax, PAGE_DIR_TABLE_POS
+   add eax, 0x1000 			     ; 此时eax为第一个页表的位置及属性
+   mov ebx, eax				     ; 此处为ebx赋值，是为.create_pte做准备，ebx为基址。
+
+;   下面将页目录项0和0xc00都存为第一个页表的地址，
+;   一个页表可表示4MB内存,这样0xc03fffff以下的地址和0x003fffff以下的地址都指向相同的页表，
+;   这是为将地址映射为内核地址做准备
+   or eax, PG_US_U | PG_RW_W | PG_P	     ; 页目录项的属性RW和P位为1,US为1,表示用户属性,所有特权级别都可以访问.
+   mov [PAGE_DIR_TABLE_POS + 0x0], eax       ; 第1个目录项,在页目录表中的第1个目录项写入第一个页表的位置(0x101000)及属性(7)
+   mov [PAGE_DIR_TABLE_POS + 0xc00], eax     ; 一个页表项占用4字节,0xc00表示第768个页表占用的目录项,0xc00以上的目录项用于内核空间,
+					     ; 也就是页表的0xc0000000~0xffffffff共计1G属于内核,0x0~0xbfffffff共计3G属于用户进程.
+   sub eax, 0x1000
+   mov [PAGE_DIR_TABLE_POS + 4092], eax	     ; 使最后一个目录项指向页目录表自己的地址
+
+;下面创建页表项(PTE)
+   mov ecx, 256				     ; 1M低端内存 / 每页大小4k = 256
+   mov esi, 0
+   mov edx, PG_US_U | PG_RW_W | PG_P	     ; 属性为7,US=1,RW=1,P=1
+.create_pte:				     ; 创建Page Table Entry
+   mov [ebx+esi*4],edx			     ; 此时的ebx已经在上面通过eax赋值为0x101000,也就是第一个页表的地址 
+   add edx,4096
+   inc esi
+   loop .create_pte
+
+;创建内核其它页表的PDE
+   mov eax, PAGE_DIR_TABLE_POS
+   add eax, 0x2000 		     ; 此时eax为第二个页表的位置
+   or eax, PG_US_U | PG_RW_W | PG_P  ; 页目录项的属性US,RW和P位都为1
+   mov ebx, PAGE_DIR_TABLE_POS
+   mov ecx, 254			     ; 范围为第769~1022的所有目录项数量
+   mov esi, 769
+.create_kernel_pde:
+   mov [ebx+esi*4], eax
+   inc esi
+   add eax, 0x1000
+   loop .create_kernel_pde
+   ret
+
