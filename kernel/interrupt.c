162626eab1a87ff436d219c5fbd8b41cf0b64749
Kinvy <kinvy66@163.com>
2023-11-01 14:55:54 +0800

    feat: 多线程调度； bug: 触发#GP异常

--- kernel/interrupt.c
+++ kernel/interrupt.c
@@ -11,9 +11,8 @@
 
 #define IDT_DESC_CNT 0x21      // 目前总共支持的中断数
 
-#define EFLAGS_IF  0x00000200
+#define EFLAGS_IF   0x00000200       // eflags寄存器中的if位为1
 #define GET_EFLAGS(EFLAG_VAR) asm volatile("pushfl; popl %0" : "=g" (EFLAG_VAR))
-
 
 /*中断门描述符结构体*/
 struct gate_desc {
@@ -77,9 +76,27 @@ static void general_intr_handler(uint8_t vec_nr) {
    if (vec_nr == 0x27 || vec_nr == 0x2f) {	// 0x2f是从片8259A上的最后一个irq引脚，保留
       return;		//IRQ7和IRQ15会产生伪中断(spurious interrupt),无须处理。
    }
-   put_str("int vector: 0x");
-   put_int(vec_nr);
-   put_char('\n');
+  /* 将光标置为0,从屏幕左上角清出一片打印异常信息的区域,方便阅读 */
+   set_cursor(0);
+   int cursor_pos = 0;
+   while(cursor_pos < 320) {
+      put_char(' ');
+      cursor_pos++;
+   }
+
+   set_cursor(0);	 // 重置光标为屏幕左上角
+   put_str("!!!!!!!      excetion message begin  !!!!!!!!\n");
+   set_cursor(88);	// 从第2行第8个字符开始打印
+   put_str(intr_name[vec_nr]);
+   if (vec_nr == 14) {	  // 若为Pagefault,将缺失的地址打印出来并悬停
+      int page_fault_vaddr = 0; 
+      asm ("movl %%cr2, %0" : "=r" (page_fault_vaddr));	  // cr2是存放造成page_fault的地址
+      put_str("\npage fault addr is ");put_int(page_fault_vaddr); 
+   }
+   put_str("\n!!!!!!!      excetion message end    !!!!!!!!\n");
+  // 能进入中断处理程序就表示已经处在关中断情况下,
+  // 不会出现调度进程的情况。故下面的死循环不会再被中断。
+   while(1);
 }
 
 /* 完成一般中断处理函数注册及异常名称注册 */
@@ -115,7 +132,6 @@ static void exception_init(void) {			    // 完成一般中断处理函数注册
    intr_name[19] = "#XF SIMD Floating-Point Exception";
 
 }
-
 
 /* 开中断并返回开中断前的状态*/
 enum intr_status intr_enable() {
@@ -155,6 +171,13 @@ enum intr_status intr_get_status() {
    return (EFLAGS_IF & eflags) ? INTR_ON : INTR_OFF;
 }
 
+/* 在中断处理程序数组第vector_no个元素中注册安装中断处理程序function */
+void register_handler(uint8_t vector_no, intr_handler function) {
+/* idt_table数组中的函数是在进入中断后根据中断向量号调用的,
+ * 见kernel/kernel.S的call [idt_table + %1*4] */
+   idt_table[vector_no] = function; 
+}
+
 /*完成有关中断的所有初始化工作*/
 void idt_init() {
    put_str("idt_init start\n");
@@ -167,4 +190,3 @@ void idt_init() {
    asm volatile("lidt %0" : : "m" (idt_operand));
    put_str("idt_init done\n");
 }
-
